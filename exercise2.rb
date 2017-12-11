63b11fb06693fdef276ed2773ffeb4d823c1977a
vioif <ireneaflores3@gmail.com>
2017-12-11 15:26:35 -0500

    first problem solved

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+# How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+
+meal_cost = 55.0
+tip_present = 20
+
+tip_value = meal_cost * tip_present/100
+
+puts tip_value
+
+#Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+
+
+#Try outputting the result of 45628 multiplied by 7839 in a section by using string interpolation.
+#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)
