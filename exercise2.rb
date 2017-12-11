247a8bd74c087dc2c56d0345b56a1569e9bc8448
vioif <ireneaflores3@gmail.com>
2017-12-11 15:59:14 -0500

    second and third problem solved

--- exercise2.rb
+++ exercise2.rb
@@ -9,6 +9,12 @@ puts tip_value
 
 #Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
 
+a = "3"
+b = "2"
+
+puts a+b # 32
 
 #Try outputting the result of 45628 multiplied by 7839 in a section by using string interpolation.
+
+puts "#{45628*7839}"
 #What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)
